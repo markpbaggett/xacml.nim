5054a8e8d131ffb1968f15f821c6036f0d2ceaf3
Mark Baggett <mbagget1@utk.edu>
2020-04-20 17:05:50 -0400

    Start framework for separating roles and logins.

--- src/xacml.nim
+++ src/xacml.nim
@@ -31,12 +31,32 @@ proc get_attribute_value(xml, target_attribute: string): string =
     if value.contains(target_attribute):
       return value.split("=")[1].replace("\"", "")
 
+proc build_conditions(xml: string): (seq[string], seq[string]) =
+  var
+    logins: seq[string]
+    roles: seq[string]
+  let
+    conditions_list = get_node(xml, "Condition")
+  for condition in conditions_list:
+    let
+      designator_node = get_node(condition, "SubjectAttributeDesignator")
+      condition_id_one = get_attribute_value(designator_node[0], "AttributeId")
+      condition_id_two = get_attribute_value(designator_node[1], "AttributeId")
+    echo condition_id_one
+    echo condition_id_two
+    logins.add(condition_id_one)
+    roles.add(condition_id_two)
+  (logins, roles)
+
 proc newXACMLRule(rule: string): XACMLRule =
+  let roles_and_logins = build_conditions(rule)
   XACMLRule(
     body: rule,
-    rule_id: get_attribute_value(rule, "RuleId"),
+    rule_id: get_attribute_value(rule, "RuleId").replace("\n", ""),
     effect: get_attribute_value(rule, "Effect").replace(">", "").replace("<Target", "").replace("\n", ""),
-    target_actions: build_actions(rule)
+    target_actions: build_actions(rule),
+    excepted_logins: roles_and_logins[0],
+    excepted_roles: roles_and_logins[1]
   )
 
 proc parse_rules*(response: string): seq[XACMLRule] =
@@ -52,4 +72,5 @@ when isMainModule:
     test_file = readFile("/home/mark/nim_projects/xacml/xml/restrict_management.xml")
     rules = parse_rules(test_file)
   for rule in rules:
-    echo rule.target_actions
+    echo rule.rule_id
+    # echo fmt"{rule.rule_id} implemnets {rule.effect} effect. {'\n'} If the user is logged in as one of the following, there is an exception:{'\n'}{'\t'}{rule.excepted_logins}{'\n'}{'\n'}Similarly, if the user has one of these roles there is an exception:{'\n'}{'\t'}{rule.excepted_roles}"
